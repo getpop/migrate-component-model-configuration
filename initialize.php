8b0f9b68c5613af2b63151f4467aa6747e8113d7
Leo <losoviz@gmail.com>
2019-07-22 12:17:46 +0530

    Extend component to initialize plugin

--- initialize.php
+++ initialize.php
@@ -1,2 +1,17 @@
-<?php 
-require_once 'migrate/pop-configurationengine.php';
+<?php
+$class = (new class() extends \PoP\Root\Component\AbstractComponent
+{
+    /**
+     * Boot component
+     *
+     * @return void
+     */
+    public static function boot()
+    {
+        parent::boot();
+
+        // Initialize code
+        require_once 'migrate/pop-configurationengine.php';  
+    }
+});
+$class::init();
